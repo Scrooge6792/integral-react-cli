c80612185cba3b8c90c0d9b4c6aa25a4c6326d93
zyj <2335561220@qq.com>
2019-10-23 11:01:57 +0800

    解决history middleware不生效的bug

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
 const path = require('path')
 const express = require('express')
-const webpack = require('webpack')
 const config = require('config')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-const proxy = require('http-proxy-middleware')
 const webpackConfig = require('../config/webpack.dev')
 
 const PORT = process.env.PORT || config.get('HMR_PORT')
@@ -13,9 +9,9 @@ const host =
 const BUILD_PATH = path.resolve(__dirname, '../build')
 
 const app = express()
-const compiler = webpack(webpackConfig)
+const compiler = require('webpack')(webpackConfig)
 
-const authProxy = proxy({
+const authProxy = require('http-proxy-middleware')({
   target: host,
   changeOrigin: true,
 })
@@ -24,18 +20,18 @@ app.engine('.html', require('ejs').__express)
 app.set('views', BUILD_PATH)
 app.set('view engine', 'html')
 
-app.use(webpackDevMiddleware(compiler, {
+/* history api组件必须先被使用，否则会不生效 */
+app.use('/', require('connect-history-api-fallback')())
+app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
 }))
-app.use(webpackHotMiddleware(compiler, {
+app.use(require('webpack-hot-middleware')(compiler, {
   path: '/__webpack_hmr',
 }))
 app.use(express.static(BUILD_PATH))
 app.use('*', authProxy)
-app.use(require('connect-history-api-fallback')())
 
 app.get('/', (req, res) => {
-  console.log('GET request')
   res.render(BUILD_PATH)
 })
 
